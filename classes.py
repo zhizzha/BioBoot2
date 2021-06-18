c2056349a69a5d7efc76c217d86b3d456118e7b6
osminogr <osminogr@yandex.ru>
2021-06-18 21:57:08 +0300

    Aln

---
+++ classes.py
@@ -0,0 +1,4 @@
+class Frame():
+    def __init__(self,length,spos = 0):
+        self.length = length
+        self.spos = spos
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from Bio import SeqIO
+from Bio import SeqIO,AlignIO
+from Bio.Align import AlignInfo
+from Bio.SubsMat import FreqTable
 import argparse as ap
-import sys
+import sys,os
 from defs import *
-#TODO Выбрать и скомпонавать последовательности по Segment
-#TODO Выравнять последовательности друг на друга
+from classes import Frame
 #TODO Посчитать консервативность каждого учатска
 #TODO (Сит) Провести анализ BLAST
 #TODO Красиво оформить :> (GUI)
-
+segments = ""
 
 
 
@@ -21,11 +22,12 @@ def createParser ():
 if __name__ == '__main__':
     parser = createParser()
     namespace = parser.parse_args(sys.argv[1:])
+    #Processing raw fasta into row segments
     with open(namespace.file) as handle:
         reqs = list(SeqIO.parse(handle, "fasta"))
         segments = list(set([record.id.split('|')[0] for record in reqs]))
         print("\nSegments in file: {}".format('/'.join(segments)))
-        print("Segment count: {}\n".format(len(segments)))
+        print("Segment count: {}".format(len(segments)))
         for segment in segments:
             print("Processing segment {}...".format(segment))
             records = []
@@ -34,3 +36,40 @@ if __name__ == '__main__':
                     records.append(record)
             print("Records count: {}\n".format(len(records)))
             SeqIO.write(records,segment+'.fasta','fasta')
+    """
+    #alaignment
+    files = [i + '.fasta' for i in segments]
+    print("List of files to align: \n{}\n".format(' | '.join(files)))
+
+    for file in files:
+        print("aligning {}".format(file))
+        command = 'mafft --quiet --auto --thread -1 {0} > ./aln/aln_{0}'.format(file)
+        os.system(command)
+    """
+    #conservation
+    files = ['./aln/aln_{}.fasta'.format(i) for i in segments]
+    print("List of files to processing: \n{}\n".format(' | '.join(files)))
+    seqs = []
+    for file in files:
+        aln = AlignIO.read(file,"fasta")
+        summary_align = AlignInfo.SummaryInfo(aln)
+        summary_align1 = AlignInfo.SummaryInfo(aln)
+        for i in range(20,21):
+            consensus = summary_align.dumb_consensus()
+            c = 0
+            for j in range(0,len(consensus) - i):
+                frame = consensus[j:j+i]
+                if not("X" in frame):
+                    expect_freq = FreqTable.FreqTable({"a": 0.25, "g": 0.25, "t": 0.25, "c": 0.25},FreqTable.FREQ)
+                    info_content = summary_align.information_content(
+                        j, j+i, e_freq_table=expect_freq, chars_to_ignore=["N","X","y","r"]
+                    )
+                    if info_content/i >= 2:
+                        #print('Seq : {0} Score: {1}'.format(frame,info_content/i))
+                        print(frame)
+                        seqs.append(frame)
+                        c +=1
+            print("Count for {0} in {1}: {2}".format(file,i,c))
+    print(len(seqs))
+    seqs = list(set(seqs))
+    print(len(seqs))
